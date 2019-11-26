6ac4c38650445b2a7959805c5b5ede361dfa2d51
Jared Salazar Martinez <tipycsnow@gmail.com>
2019-11-26 16:09:14 -0600

    Trabajo

--- registroProceso.php
+++ registroProceso.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Sign Up</title>
+</head>
+<body>
 <?php
     include("conexion.php");
 
@@ -12,9 +20,39 @@
     $resultado = $conexionDB->query($statement);
 
     if($resultado){
-        echo "Si se inserto el registro";
+        echo "<nav class=\"navbar sticky-top navbar-expand-lg navbar-dark bg-dark bg-light\">
+        <a class=\"navbar-brand\" href=\"#\">
+            <img src=\"img/cintillo-web.png\" height=\"30\" class=\"d-inline-block align-top\" alt=\"\">
+        </a>
+        <div class=\"collapse navbar-collapse\">
+            <ul class=\"navbar-nav mr-auto\">
+                <li class=\"nav-item active\">
+                    <a class=\"nav-link\" href=\"index.html\">Home<span class=\"sr-only\">(current)</span></a>
+                </li>
+                <li class=\"nav-item\">
+                    <a class=\"nav-link\" href=\"login.php\">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+        
+        <br><br><br>
+        <section class=\"container\">
+            <section class=\"row\">
+                <div class=\"col-md-4\"></div>
+                <div class=\"col-md-4\">
+                    <div class=\"jumbotron\">
+                        <h2 class=\"display-5\">Registro exitoso</h2>
+                        <a type=\"button\" href=\"login.php\" class=\"btn btn-primary\">Login</a>
+                    </div>
+                </div>
+                <div class=\"col-md-4\"></div>
+            </section>
+        </section>";
     }
     else{
         echo "No se inserto el registro";
     }
 ?>
+</body>
+</html>
